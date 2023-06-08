663f6d15835086e62710076e81e21ba43f86be6a
evgenii.saurov <evgenii.saurov@supperundsupper.com>
2023-06-08 23:01:08 +0200

    add field from API

--- lib/src/widget/mobile/magazine_item_widget.dart
+++ lib/src/widget/mobile/magazine_item_widget.dart
@@ -204,7 +204,7 @@ class MagazineItemWidget extends StatelessWidget {
                 color: AppColor.cyan,
                 borderRadius: BorderRadius.circular(8),
                 onTap: () async {
-                  var url = 'https://t.me/publishpoint';
+                  var url = data.submitPaperLink;
                   if (await canLaunchUrl(Uri.parse(url))) {
                     await launchUrl(
                       Uri.parse(url),
